13f33d86f3737b40fbfd2a6f8987a87422173adf
smallg0at <10290487+smallg0at@user.noreply.gitee.com>
2022-01-13 14:52:43 +0800

    Add 2 Students' work

---
+++ projects/zhangyaozhong/img/albumscript.js
@@ -0,0 +1,47 @@
+const prev = document.querySelector("#prev");
+const next = document.querySelector("#next");
+
+const slides = document.querySelectorAll(
+  ".slide"
+);
+
+var currentIndex = 0;
+var autoPlay = true;
+var forward = true;
+var interval = 8000;
+next.addEventListener("click", handleNextClicked);
+prev.addEventListener("click", handlePrevClicked);
+
+if (autoPlay) {
+  setInterval(
+    forward
+      ? handleNextClicked
+      : handlePrevClicked,
+    interval
+  );
+}
+
+function handleNextClicked() {
+  let current = slides[currentIndex];
+  current.classList.remove("current");
+
+  currentIndex++;
+  if (currentIndex >= slides.length){
+    currentIndex = 0;
+  }
+
+  slides[currentIndex].classList.add("current");
+}
+
+function handlePrevClicked(){
+  let current = slides[currentIndex];
+  current.classList.remove("current");
+
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = slides.length - 1;
+  }
+
+  slides[currentIndex].classList.add("current");
+}
+
