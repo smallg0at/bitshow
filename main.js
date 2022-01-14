a91f6fa997910ce36c4240dd7fdf563e5e858f4d
smallg0at <10290487+smallg0at@user.noreply.gitee.com>
2022-01-14 22:43:01 +0800

    Config - Add 4 works

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ function renderList() {
     newElement.classList.add("card");
     newElement.setAttribute("role", "button");
     newElement.setAttribute("tabindex", "0");
-    newElement.innerHTML = `<img src="./assets/siteshots/${item.img}" alt="">
+    newElement.innerHTML = `<img src="${item.img}" alt="">
         <span class="imglabel" title="代码行数">
             <span class="main">${item.name}</span>
             <span class="loc">${item.loc || "不明"}</span>
